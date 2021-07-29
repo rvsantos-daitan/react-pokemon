0c4135e6a794fdf728cab650b26218200e99c149
Raphael Viana Dos Santos <rvsantos@daitan.com>
2021-07-29 10:19:04 -0300

    Adjusted Snapshots/ Fixed Provider from Pages/

--- src/containers/filters/pokemonfilter.stories.tsx
+++ src/containers/filters/pokemonfilter.stories.tsx
@@ -1,5 +1,5 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Provider } from "react-redux";
 import { store } from "../../store";
 
--- src/pages/main.stories.tsx
+++ src/pages/main.stories.tsx
@@ -1,5 +1,5 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import { Provider } from "react-redux";
 import { store } from "../store";
 
@@ -8,7 +8,7 @@ import Main from "./main";
 export default {
     component: Main,
     title: "Pages/Main Page",
-    decoratos: [story => <Provider store={store}>{story()}</Provider>]
+    decorators: [(story) => <Provider store={store}>{story()}</Provider>]
 } as ComponentMeta<typeof Main>
 
 const Template: ComponentStory<typeof Main> = args => <Main {...args}/>
